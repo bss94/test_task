d7dba9abe5d586da33525789dc9b90a4ba01863d
Sergey Brazhnikov <serjak.2012@gmail.com>
2025-04-18 16:37:53 +0600

    add route page not found

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Layout from './UI/Layout/Layout.tsx';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Users from './features/Users/Users.tsx';
 import OneUser from './features/Users/OneUser.tsx';
+import { Typography } from '@mui/material';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Navigate to={'/users'} />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:id" element={<OneUser />} />
+        <Route path="*" element={<Typography variant="h2">Page Not Found!</Typography>} />
       </Routes>
     </Layout>
   );
